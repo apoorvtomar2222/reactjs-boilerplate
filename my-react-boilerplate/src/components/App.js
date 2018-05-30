34c0fd6a9c2904ebff6e59bdc02896b443f64597
Apoorv Tomar <apoorv_tomar2222@yahoo.com>
2018-05-30 15:44:05 +0530

    first commit

---
+++ my-react-boilerplate/src/components/App.js
@@ -0,0 +1,11 @@
+import react, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome Guys</h1>
+            </div>
+        )
+    }
+}
+export default App;
---
+++ my-react-boilerplate/test/App.test.js
@@ -0,0 +1,3 @@
+import App from '../src/components/App'
+import React from 'react';
+import { shallow } from 'enzyme'
