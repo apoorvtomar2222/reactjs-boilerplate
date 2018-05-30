4bdc4d6637450ed9a64bc8013328412a5ea5a541
Apoorv Tomar <apoorv_tomar2222@yahoo.com>
2018-05-30 16:27:27 +0530

    New Test Added Jest Enzyme

--- my-react-boilerplate/test/App.test.js
+++ my-react-boilerplate/test/App.test.js
@@ -1,3 +1,12 @@
 import App from '../src/components/App'
 import React from 'react';
 import { shallow } from 'enzyme'
+
+describe('App', () => {
+    test('Should match snapshot', () => {
+        const wrapper = shallow(<App />)
+        console.log('wrapper', wrapper)
+        expect(wrapper.find('h1').text()).toBe('Welcome Guys')
+        expect(wrapper).toMatchSnapshot
+    })
+})
